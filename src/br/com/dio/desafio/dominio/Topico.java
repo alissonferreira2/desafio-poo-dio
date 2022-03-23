c0887c98bfcf8834354d401c1daed38e019cebc9
Alisson Ferreira <101062903+alissonferreira2@users.noreply.github.com>
2022-03-23 11:06:33 -0300

    Create Topico.java
    Adicionando a classe Topico

---
+++ src/br/com/dio/desafio/dominio/Topico.java
@@ -0,0 +1,22 @@
+package br.com.dio.desafio.dominio;
+
+public class Topico {
+	private String titulo;
+	private String texto;
+	
+	public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+}
